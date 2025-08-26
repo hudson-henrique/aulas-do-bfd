6ab823869e74f2eab406e1f014bdbc960b0d4fe8
hudson-henrique <amorimhenrique252@gmail.com>
2025-08-26 15:28:47 -0300

    oi

---
+++ aula7.js
@@ -0,0 +1,17 @@
+let TimesDeFutebol = ["Sport","Bahia","Palmeiras", "Vasco"];
+
+console.log ("primeiro time:", TimesDeFutebol[0]);
+
+console.log("ultimo time:", TimesDeFutebol[TimesDeFutebol.length - 1]);
+  
+
+let minhaListaDeTarefas = [];
+
+minhaListaDeTarefas.push("ir para a escola");
+minhaListaDeTarefas.push("jogar o lixo");
+minhaListaDeTarefas.push("academia");
+minhaListaDeTarefas.push("futebol");
+
+console.log("Lista de tarefas:", minhaListaDeTarefas)
+
+
