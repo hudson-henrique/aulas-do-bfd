1beff5076a4fb960aff5661a7ba3363e326fd347
hudson-henrique <amorimhenrique252@gmail.com>
2025-08-26 16:08:09 -0300

    primeiro commit

--- aula7.js
+++ aula7.js
@@ -1,10 +1,12 @@
+//atvd1
+
 let TimesDeFutebol = ["Sport","Bahia","Palmeiras", "Vasco"];
 
 console.log ("primeiro time:", TimesDeFutebol[0]);
 
 console.log("ultimo time:", TimesDeFutebol[TimesDeFutebol.length - 1]);
   
-
+//atvd2
 let minhaListaDeTarefas = [];
 
 minhaListaDeTarefas.push("ir para a escola");
@@ -14,4 +16,18 @@ minhaListaDeTarefas.push("futebol");
 
 console.log("Lista de tarefas:", minhaListaDeTarefas)
 
+//atvd3
+let animais = ["elefante", "cachorro", "leão", "tigre"];
 
+for (let i = 0; i < animais.length; i++) {
+  console.log(`O animal na posição ${i} é ${animais[i]}`);
+}
+
+//atvd4
+let numeros = [7, 5, 15, 22, 11, 4];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    console.log(numeros[i]);
+  }
+}
